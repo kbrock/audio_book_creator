34769e5d7a26c8c110d874ff4e7213afa650760e
Keenan Brock <keenan@thebrocks.net>
2015-03-07 17:08:54 -0500

    use url filter constructor not host=

--- spec/audio_book_creator/url_filter_spec.rb
+++ spec/audio_book_creator/url_filter_spec.rb
@@ -6,45 +6,44 @@ describe AudioBookCreator::UrlFilter do
   context "#host" do
     subject { described_class.new(nil) }
 
-    it "supports strings (and nils)" do
-      subject.host = "http://site.com/page"
-      expect(subject.host).to eq("site.com")
+    it { expect(subject.host).to be_nil }
 
-      subject.host = nil
-      expect(subject.host).to be_nil
+    it "supports strings (and nils)" do
+      subject = described_class.new("http://site.com/page")
+      expect(subject.host).to eq("site.com")
     end
 
     it "supports uris" do
-      subject.host = uri("page")
+      subject = described_class.new(uri("page"))
       expect(subject.host).to eq(uri("page").host)
     end
-  end
 
-  it "spiders without a host" do
-    subject.host = nil
-    expect(subject.include?(uri("good"))).not_to be_truthy
-    expect(subject.include?(uri("http://anothersite.com/bad"))).not_to be_truthy
-    expect(subject.include?(uri("http://anothersite.com/bad"))).not_to be_truthy
+    it "spiders without a host" do
+      expect(subject.include?(uri("good"))).not_to be_truthy
+      expect(subject.include?(uri("http://anothersite.com/bad"))).not_to be_truthy
+    end
   end
 
   it "spiders local pages only" do
     expect(subject.include?(uri("page1"))).not_to be_truthy
     expect(subject.include?(uri("good"))).not_to be_truthy
     url = uri("http://anothersite.com/bad")
+    # odd syntax because we are testing the return value of the block passed to logger.warn
     expect(subject.logger).to receive(:warn) { |&arg| expect(arg.call).to eq("ignoring remote url #{url}") }
     expect { subject.include?(url) }.to raise_error("remote url #{url}")
   end
 
   context "visit with #extensions" do
     %w(/page / .html .php .jsp .htm).each do |ext|
-      it "visits #{ext}" do
+      it "visits page2#{ext}" do
         expect(subject.include?(uri("page2#{ext}"))).not_to be_truthy
       end
     end
 
     %w(.jpg .png .js).each do |ext|
-      it "doesnt visit #{ext}" do
+      it "doesnt visit page2#{ext}" do
         url = uri("page2#{ext}")
+        # odd syntax because we are testing the return value of the block passed to logger.warn
         expect(subject.logger).to receive(:warn) { |&arg| expect(arg.call).to eq("ignoring bad file extension #{url}") }
         expect { subject.include?(url) }.to raise_error("bad file extension")
       end
