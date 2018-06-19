d273cf0d0d03fba02dddbd7e84c4201b25fcbb96
Keenan Brock <keenan@thebrocks.net>
2018-06-19 16:57:35 -0400

    add created_at column

--- lib/audio_book_creator/page_db.rb
+++ lib/audio_book_creator/page_db.rb
@@ -7,17 +7,28 @@ module AudioBookCreator
   class PageDb
     include Enumerable
 
-    attr_accessor :filename, :table_name, :encode
+      STANDARD_FIELDS = {
+        "name"       => "text",
+        "contents"   => "blob",
+        "created_at" => "datetime"
+      }.freeze
 
-    def initialize(filename, table_name, encode)
+    # filename - filename for our data (currently just use pages.db)
+    # table_name - 2 instances of this class, settings and
+    # encode - whether to use json to encode the value (we do this for settings)
+    # fields - fields in the database
+    attr_accessor :filename, :table_name, :encode, :fields
+
+    def initialize(filename, table_name, encode, fields = STANDARD_FIELDS)
       @filename = filename
       @table_name = table_name
       @encode = encode
+      @fields = fields
     end
 
     def []=(key, value)
       value = JSON.generate(value) if encode && value
-      db.execute "insert into #{table_name} (name, contents) values (?, ?)", [key, value]
+      db.execute "insert into #{table_name} (name, contents, created_at) values (?, ?, ?)", [key, value, Time.now.utc.to_s]
     end
 
     def [](key)
@@ -37,6 +48,10 @@ module AudioBookCreator
       db.execute "delete from pages where name like ?", like_name
     end
 
+    def date(key)
+      db.execute("select created_at from #{table_name} where name = ?", key).map { |row| row.first }.first
+    end
+
     private
 
     def db
@@ -45,7 +60,18 @@ module AudioBookCreator
 
     def create
       SQLite3::Database.new(filename).tap do |db|
-        db.execute("create table if not exists #{table_name} (name text, contents blob)")
+        column_sql = fields.map { |n,t| "#{n} #{t}" }.join(", ")
+        db.execute("create table if not exists #{table_name} (#{column_sql})")
+
+        # list of columns in the table
+        # sql returns:
+        # 0|name|text|0||0
+        # 1|contents|blob|0||0
+        columns = db.table_info(table_name).map { |row| row["name"] }
+        # make sure all the columns we want are in the table
+        fields.each do |name, type|
+          db.execute("alter table #{table_name} add column #{name} #{type}") unless columns.include?(name)
+        end
       end
     end
   end
